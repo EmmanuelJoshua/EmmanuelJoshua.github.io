92ed0846c9fde2a0e5e4726a68dbbd37a90c2a9b
Emmanuel Joshua <emmanueljoshua145@gmail.com>
2021-03-06 12:49:04 +0100

    Some animations

--- js/script.js
+++ js/script.js
@@ -12,3 +12,30 @@ $(window).scroll(function () {
     }
 
 });
+
+var drawRipple = function (ev) {
+    var x = ev.clientX;
+    var y = ev.clientY;
+    var node = document.querySelector(".ripple");
+    var newNode = node.cloneNode(true);
+    newNode.classList.add("animate");
+    newNode.style.left = ev.clientX - 5 + "px";
+    newNode.style.top = ev.clientY - 5 + "px";
+    node.parentNode.replaceChild(newNode, node);
+};
+
+// var drawTouchRipple = function (event) {
+//     // ev.preventDefault();
+//     var x = event.touches[0].clientX;
+//     var y = event.touches[0].clientY;
+//     var node = document.querySelector(".ripple");
+//     var newNode = node.cloneNode(true);
+//     newNode.classList.add("animate");
+//     newNode.style.left = ev.clientX - 5 + "px";
+//     newNode.style.top = ev.clientY - 5 + "px";
+//     node.parentNode.replaceChild(newNode, node);
+// };
+
+//Ripple Triggers
+window.addEventListener("click", drawRipple);
+// window.addEventListener("touchstart", drawTouchRipple, );

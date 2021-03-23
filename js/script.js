fa062e28da94c0d148156fe2ef1c7061ef13635d
Emmanuel Joshua <emmanueljoshua145@gmail.com>
2021-03-23 10:46:05 +0100

    Hotfix

--- js/script.js
+++ js/script.js
@@ -1,25 +1,21 @@
-let navIcon = document.getElementById('nav-icon1');
-let nav = document.getElementsByTagName('nav');
-
-document.onreadystatechange = function () {
-    if (document.readyState == 'interactive') {
-        navIcon.addEventListener("click", function () {
-            this.classList.toggle('open');
-            if (!nav[0].classList.contains('scrolled'))
-                nav[0].classList.add('scrolled');
-        });
-    }
-}
-
-window.onscroll = function () {
-    "use strict";
-    if (navIcon.classList.contains('open')) {
+$(document).ready(function () {
+    $('#nav-icon1').click(function () {
+        $(this).toggleClass('open');
+        if (!$('nav').hasClass('scrolled')) $('nav').addClass('scrolled');
+    });
+});
+$(window).scroll(function () {
+    if ($('#nav-icon1').hasClass('open')) {
 
     } else {
-        nav[0].classList.toggle('scrolled', document.body.scrollTop > 50 || document.documentElement.scrollTop > 50);
+        $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
     }
-}
 
+});
+
+var arr = [1, 2, 3, 4, 5, 6];
+
+$('#myCarousel').carousel();
 
 var drawRipple = function (ev) {
     var x = ev.clientX;
@@ -32,5 +28,40 @@ var drawRipple = function (ev) {
     node.parentNode.replaceChild(newNode, node);
 };
 
+// var drawTouchRipple = function (event) {
+//     // ev.preventDefault();
+//     var x = event.touches[0].clientX;
+//     var y = event.touches[0].clientY;
+//     var node = document.querySelector(".ripple");
+//     var newNode = node.cloneNode(true);
+//     newNode.classList.add("animate");
+//     newNode.style.left = ev.clientX - 5 + "px";
+//     newNode.style.top = ev.clientY - 5 + "px";
+//     node.parentNode.replaceChild(newNode, node);
+// };
+
 //Ripple Triggers
 window.addEventListener("click", drawRipple);
+// window.addEventListener("touchstart", drawTouchRipple, );
+
+// let navIcon = document.getElementById('nav-icon1');
+// let nav = document.getElementsByTagName('nav');
+
+// document.onreadystatechange = function () {
+//     if (document.readyState == 'interactive') {
+//         navIcon.addEventListener("click", function () {
+//             this.classList.toggle('open');
+//             if (!nav[0].classList.contains('scrolled'))
+//                 nav[0].classList.add('scrolled');
+//         });
+//     }
+// }
+
+// window.onscroll = function () {
+//     "use strict";
+//     if (navIcon.classList.contains('open')) {
+
+//     } else {
+//         nav[0].classList.toggle('scrolled', document.body.scrollTop > 50 || document.documentElement.scrollTop > 50);
+//     }
+// }
